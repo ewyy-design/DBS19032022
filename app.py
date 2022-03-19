e2646c607e442b2d93bd26a1af08e4a3058022ff
Eunice.W <73635385+ewyy-design@users.noreply.github.com>
2022-03-19 17:24:40 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[109]:
+
+
+from flask import Flask, request, render_template
+
+
+# In[110]:
+
+
+app = Flask(__name__)
+
+
+# In[111]:
+
+
+import joblib
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        rates = request.form.get('rates')
+        print(rates)
+        model = joblib.load("DBS_Reg")
+        pred = model.predict([[float(rates)]])
+        s = "The predicted DBS share price is" + str(pred)
+        return(render_template("index.html", results=s))
+    else:
+        return(render_template("index.html", results="2"))
+
+
+# In[ ]:
+
+
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
